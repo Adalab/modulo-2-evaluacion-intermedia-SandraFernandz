0eb73d018e0dd46e9c92e881957a68b4d15ac470
Sandra Fernandz <sandraenmunich@gmail.com>
2021-09-13 08:25:28 +0200

    securing changes made yesterday

--- js/main.js
+++ js/main.js
@@ -5,44 +5,47 @@ const numberInput = document.querySelector('.js_number');
 const hint = document.querySelector('.js_tries');
 const counter = document.querySelector('.js_counter');
 
-//función que crea un número random 
+//función que crea un número random
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-    }
+  return Math.ceil(Math.random() * max);
+}
 
 //recogemos la función de crear random number en una variable y le pasamos el parámetro del número máximo del juego (no pasamos el parámetro en la función handler porque si no se recargaría un nuevo número aleatorio cada vez que cargásemos la página)
 const randomNum = getRandomNumber(100);
-    console.log(randomNum);
+console.log(randomNum);
 
+//función que convierte hint.innerHTML en una variable usersHinty lo convierte en una variable usersHint. Usamos la función en el condicional de la función inmediatamente después.
 
-
-//función que genera pistas del número aleatorio creado, según el número que introduce el usuario 
-function guessNumber(){
-    if(numberInput.value < 0 || numberInput.value >= 101){
-        hint.innerHTML = 'Por favor, introduce un número entre 0 y 100'
-    }else if(randomNum === parseInt(numberInput.value)){
-        hint.innerHTML = '¡Felicidades!'
-    }else if(randomNum > numberInput.value){
-        hint.innerHTML = 'El número es demasiado bajo, inténtalo de nuevo'
-    }else if(randomNum < numberInput.value){
-        hint.innerHTML = 'El número es demasiado alto, prueba otra vez'
-    }
+function writeHint(message) {
+  hint.innerHTML = message;
 }
 
-//función que cuenta los intentos 
+//función que genera pistas del número aleatorio creado, según el número que introduce el usuario
+function guessNumber() {
+  if (numberInput.value < 0 || numberInput.value >= 101) {
+    writeHint('Por favor, introduce un número entre 0 y 100');
+  } else if (randomNum === parseInt(numberInput.value)) {
+    writeHint('¡Felicidades!');
+  } else if (randomNum > numberInput.value) {
+    writeHint('El número es demasiado bajo, inténtalo de nuevo');
+  } else if (randomNum < numberInput.value) {
+    writeHint('El número es demasiado alto, prueba otra vez');
+  }
+}
+
+//función que cuenta los intentos
 //creamos variable fuera para que no se reescriba valor
- let count = 0;
- function counterSelect(){
-     count++;
-     counter.innerHTML = `Número de intentos: ${count}`;
+let count = 0;
+function counterSelect() {
+  count++;
+  counter.innerHTML = `Número de intentos: ${count}`;
 }
 
 //función que engloba las funciones que se desencadenan al escuchar el mismo elemento
 
- function handlerGuessNumber(){
-     guessNumber();
-     counterSelect();
-   
- }
+function handlerGuessNumber() {
+  guessNumber();
+  counterSelect();
+}
 
 update.addEventListener('click', handlerGuessNumber);
